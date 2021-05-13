c0fcd7717c2f9304fa69be5f68cfe6a2fea6ba87
Xavier Marques <xavierpmarques@gmail.com>
2021-05-13 18:35:51 +0100

    Add Mouse Hover lister in divs. Make divs change color upon hover. Center #introduction text

--- javascript/etchAsketch.js
+++ javascript/etchAsketch.js
@@ -26,4 +26,48 @@ function createHorizontalDiv() {
     return horizontalDiv
 }
 
+const squares = document.querySelectorAll(".horizontalDiv");
+squares.forEach(square => {
+    square.addEventListener("mouseenter", colorise)
+});
+    
+function colorise(e) {
+    let targetSquare = e.target;
+    let colorPick = Math.floor((Math.random() * 10));
+    switch (colorPick) {
+        case 1:
+            targetSquare.style.cssText = "background: orange;"
+            break;
+        case 2:
+            targetSquare.style.cssText = "background: yellow;"
+            break;
+        case 3:
+            targetSquare.style.cssText = "background: black;"
+            break;
+        case 4:
+            targetSquare.style.cssText = "background: pink;"
+            break;
+        case 5:
+            targetSquare.style.cssText = "background: cyan;"
+            break;
+        case 6:
+            targetSquare.style.cssText = "background: magenta;"
+            break;
+        case 7:
+            targetSquare.style.cssText = "background: brown;"
+            break;
+        case 8:
+            targetSquare.style.cssText = "background: black;"
+            break;
+        case 9:
+            targetSquare.style.cssText = "background: grey;"
+            break;
+        case 10:
+            targetSquare.style.cssText = "background: red;"
+            break;
+        default:
+            break;
+    }
+}
+
 })
