dd7e868829f8eca7624449d7cf54174941a05fac
Xavier Marques <xavierpmarques@gmail.com>
2021-05-13 22:13:21 +0100

    Add ability to reset grid
    Grid now resets after clicking the 'Reset Grid' button and inserting an integer < 100, creating a new grid X by X grid where 'X' is the userInput

--- javascript/etchAsketch.js
+++ javascript/etchAsketch.js
@@ -1,12 +1,62 @@
 document.addEventListener("DOMContentLoaded", function () {
     const container = document.querySelector("#gridContainer");
-    grid();
+    grid(16);
 
-function grid() {
-    for (i = 0; i < 16; i++) {
+const introductionId = document.querySelector("#introduction");
+
+// Listen for Clicks on "Reset Grid" Button
+const resetButton = document.querySelector("#resetButton");
+resetButton.addEventListener("click", resetPrompt);
+
+function resetPrompt() {
+    resetButton.style.cssText = "visibility: hidden";
+    // Creates Textfield for User Input and a Submit Button
+    let promptUser = document.createElement("input");
+    promptUser.placeholder = " Number '10' =  10x10 grid"
+    promptUser.classList.add("input")
+    introductionId.appendChild(promptUser);
+    let submitButton = document.createElement("button");
+    submitButton.innerText = "Create"
+    submitButton.classList.add("submitButton");
+    introductionId.appendChild(submitButton);
+    submitButton.addEventListener("click", fullReset);
+
+    function fullReset(){
+        let userInput = promptUser.value;
+        if (isNaN(userInput) || userInput > 100){
+            alert("Only numbers equal to or smaller than 100 are valid.")
+        }
+        else {
+            resetButton.style.cssText = "visibility: visible";
+            // Deletes previous grid
+            deleteGrid();
+            // Creates new grid with userInput
+            grid(userInput);
+            listenForSquareHover();
+        }
+    }    
+
+    function deleteGrid() {
+        promptUser.remove();
+        submitButton.remove();
+        let allVerticalDivs = document.querySelectorAll(".verticalDiv");
+        allVerticalDivs.forEach(verticalDiv => {
+            verticalDiv.remove();
+        });
+        let allHorizontalDivs = document.querySelectorAll(".horizontalDiv");
+        allHorizontalDivs.forEach(horizontalDiv => {
+            horizontalDiv.remove();
+        });    
+    }
+}
+
+
+// Creates a standard "number by number" grid of squares.
+function grid(number) {
+    for (i = 0; i < number; i++) {
         let verticalDiv = createVerticalDiv();
         verticalDiv.classList.add("verticalDiv")
-        for (z = 0; z < 16; z++) {
+        for (z = 0; z < number; z++) {
             let horizontalDiv = createHorizontalDiv()
             verticalDiv.appendChild(horizontalDiv);
         } 
@@ -26,11 +76,15 @@ function createHorizontalDiv() {
     return horizontalDiv
 }
 
-const squares = document.querySelectorAll(".horizontalDiv");
-squares.forEach(square => {
-    square.addEventListener("mouseenter", colorise)
-});
-    
+// Listens for hover on each div square. 
+listenForSquareHover();
+function listenForSquareHover() {
+    let squares = document.querySelectorAll(".horizontalDiv");
+    squares.forEach(square => {
+        square.addEventListener("mouseenter", colorise)
+    });
+}
+// Adds random colour to each square.
 function colorise(e) {
     let targetSquare = e.target;
     let colorPick = Math.floor((Math.random() * 10));
